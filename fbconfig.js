18fa2462fef2160ed505e8742bbd081b03b0ed7e
LMorel77 <lmorel77@aol.com>
2018-03-29 22:37:40 -0400

    Initial Commit

---
+++ fbconfig.js
@@ -0,0 +1,12 @@
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBqhH94VCk4HsDR1PCIVg7vV6h3KMHwu6w",
+    authDomain: "fir-trainactivity.firebaseapp.com",
+    databaseURL: "https://fir-trainactivity.firebaseio.com",
+    projectId: "fir-trainactivity",
+    storageBucket: "",
+    messagingSenderId: "197222933748"
+  };
+
+  firebase.initializeApp(config);
